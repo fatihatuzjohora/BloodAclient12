1648de0b1a2779c8d666e10399830a08088edd33
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-06-17 02:58:20 +0600

    role set

--- src/routes/DonorRoute.jsx
+++ src/routes/DonorRoute.jsx
@@ -1,18 +1,17 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
-import useRole from '../hook/useRole'
-import PropTypes from 'prop-types'
+import useRole from "../hook/useRole";
+import PropTypes from "prop-types";
 const HostRoute = ({ children }) => {
-  const [data] = useRole()
-const role = data.role;
+  const [data] = useRole();
+  const role = data.role;
 
+  if (role === "donor" || role === "admin") return children;
+  return <Navigate to="/dashboard" />;
+};
 
-  if (role === 'donor' || role === 'admin') return children
-  return <Navigate to='/dashboard' />
-}
-
-export default HostRoute
+export default HostRoute;
 
 HostRoute.propTypes = {
   children: PropTypes.element,
-}
+};
