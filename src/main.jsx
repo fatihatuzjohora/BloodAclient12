eda098401dafba79ca05233dc2a074fc5fcf2b13
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-06-17 02:56:56 +0600

    main set

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Routes.jsx'
-import './index.css'
-import AuthProvider from './providers/AuthProvider'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes.jsx";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}></RouterProvider>
-    <Toaster></Toaster>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+        <Toaster></Toaster>
+      </QueryClientProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
