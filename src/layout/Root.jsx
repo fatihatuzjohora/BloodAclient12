98676a0c20505eb79ce243c3fa76d062832e71f0
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-06-17 03:02:51 +0600

    some correction

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -3,19 +3,17 @@ import Navbar from "../Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
-    return (
-        <div>
-           <Navbar></Navbar>
+  return (
+    <div>
+      <Navbar></Navbar>
 
-           
-           <div >
-               <Outlet></Outlet>
-            </div>
-            
-            
-            <Footer></Footer>
-        </div>
-    );
+      <div>
+        <Outlet></Outlet>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
