54d51b8dcac05308a250b78f312301c20e45e128
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-06-17 02:56:01 +0600

    layout set

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,21 +1,20 @@
-import { Outlet } from 'react-router-dom'
-import Sidebar from '../Components/Dashboard/Sidebar/Sidebar'
-
+import { Outlet } from "react-router-dom";
+import Sidebar from "../Components/Dashboard/Sidebar/Sidebar";
 
 const DashboardLayout = () => {
-    return (
-      <div className='relative  md:flex'>
-        {/* Sidebar */}
-        <Sidebar />
-  
-        {/* Outlet --> Dynamic content */}
-        <div className='flex-1 md:ml-64'>
-          <div className='p-5'>
-            <Outlet />
-          </div>
+  return (
+    <div className="relative  md:flex">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Outlet --> Dynamic content */}
+      <div className="flex-1 md:ml-64">
+        <div className="p-5">
+          <Outlet />
         </div>
       </div>
-    )
-  }
-  
-  export default DashboardLayout
+    </div>
+  );
+};
+
+export default DashboardLayout;
