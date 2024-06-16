c25f98b786c837f6228d253ab68d07b666f3063f
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-06-17 02:57:29 +0600

    valenter set

--- src/routes/Volunteer.jsx
+++ src/routes/Volunteer.jsx
@@ -1,18 +1,18 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
-import useRole from '../hook/useRole'
-import PropTypes from 'prop-types'
+import useRole from "../hook/useRole";
+import PropTypes from "prop-types";
 
 const Volunteer = ({ children }) => {
-  const [data] = useRole()
-const role = data.role;
+  const [data] = useRole();
+  const role = data.role;
 
-  if (role === 'volunteer' || role === 'admin') return children
-  return <Navigate to='/dashboard' />
-}
+  if (role === "volunteer" || role === "admin") return children;
+  return <Navigate to="/dashboard" />;
+};
 
-export default Volunteer
+export default Volunteer;
 
 Volunteer.propTypes = {
   children: PropTypes.element,
-}
+};
